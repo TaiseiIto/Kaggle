956e0fc019506829f4bced2af3689baf3401c0b8
TaiseiIto <taisei1212@outlook.jp>
2022-12-17 21:44:17 +0900

    titanic

--- competitions/titanic/learn.py
+++ competitions/titanic/learn.py
@@ -1,3 +1,14 @@
+import pandas
+
 if __name__ == '__main__':
 	print('Hello, World!')
+	gender_submission_data_frame = pandas.read_csv('gender_submission.csv')
+	test_data_frame = pandas.read_csv('test.csv')
+	train_data_frame = pandas.read_csv('train.csv')
+	print('gender_submission_data_frame')
+	print(gender_submission_data_frame)
+	print('test_data_frame')
+	print(test_data_frame)
+	print('train_data_frame')
+	print(train_data_frame)
 
