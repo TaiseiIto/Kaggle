1f8fd48f62c02f78ef8308e5fab6ed4632c9b3c1
TaiseiIto <taisei1212@outlook.jp>
2022-12-17 22:13:20 +0900

    titanic

--- competitions/titanic/learn.py
+++ competitions/titanic/learn.py
@@ -11,4 +11,12 @@ if __name__ == '__main__':
 	print(test_data_frame)
 	print('train_data_frame')
 	print(train_data_frame)
+	gender_submission_column_names = set(gender_submission_data_frame.columns)
+	test_data_column_names = set(test_data_frame.columns)
+	print("gender_submission_column_names = {}".format(gender_submission_column_names))
+	print("test_data_column_names = {}".format(test_data_column_names))
+	output_column_names = gender_submission_column_names - test_data_column_names
+	input_column_names = test_data_column_names
+	print("output_colunm_names = {}".format(output_column_names));
+	print("input_colunm_names = {}".format(input_column_names));
 
