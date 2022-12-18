c1e5281a5b47b57635677fa2bac56870c8c0ca91
TaiseiIto <taisei1212@outlook.jp>
2022-12-18 21:10:56 +0900

    titanic

--- competitions/titanic/evaluate.py
+++ competitions/titanic/evaluate.py
@@ -22,8 +22,8 @@ gender_submission_column_names = set(gender_submission_data_frame.columns)
 test_data_column_names = set(test_data_frame.columns)
 output_column_names = gender_submission_column_names - test_data_column_names
 input_column_names = test_data_column_names
-test_input_data_frame = test_data_frame[input_column_names]
-test_output_data_frame = gender_submission_data_frame[output_column_names]
+test_input_data_frame = test_data_frame[list(input_column_names)]
+test_output_data_frame = gender_submission_data_frame[list(output_column_names)]
 with open('model.pkl', 'rb') as model_file:
 	model = pickle.load(model_file)
 score = model.score(test_input_data_frame, test_output_data_frame)
