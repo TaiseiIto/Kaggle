3b38e468d4ae752977558d05a651f27184bf1a3a
TaiseiIto <taisei1212@outlook.jp>
2022-12-18 15:43:47 +0900

    titanic

--- competitions/titanic/learn.py
+++ competitions/titanic/learn.py
@@ -1,4 +1,5 @@
 import pandas
+import lightgbm
 
 if __name__ == '__main__':
 	print('Hello, World!')
@@ -13,22 +14,25 @@ if __name__ == '__main__':
 	print(train_data_frame)
 	gender_submission_column_names = set(gender_submission_data_frame.columns)
 	test_data_column_names = set(test_data_frame.columns)
-	print("gender_submission_column_names = {}".format(gender_submission_column_names))
-	print("test_data_column_names = {}".format(test_data_column_names))
+	print('gender_submission_column_names = {}'.format(gender_submission_column_names))
+	print('test_data_column_names = {}'.format(test_data_column_names))
 	output_column_names = gender_submission_column_names - test_data_column_names
 	input_column_names = test_data_column_names
-	print("output_colunm_names = {}".format(output_column_names))
-	print("input_colunm_names = {}".format(input_column_names))
+	print('output_colunm_names = {}'.format(output_column_names))
+	print('input_colunm_names = {}'.format(input_column_names))
 	test_input_data_frame = test_data_frame[input_column_names]
 	test_output_data_frame = gender_submission_data_frame[output_column_names]
 	train_input_data_frame = train_data_frame[input_column_names]
 	train_output_data_frame = train_data_frame[output_column_names]
-	print("test_input_data_frame")
+	print('test_input_data_frame')
 	print(test_input_data_frame)
-	print("test_output_data_frame")
+	print('test_output_data_frame')
 	print(test_output_data_frame)
-	print("train_input_data_frame")
+	print('train_input_data_frame')
 	print(train_input_data_frame)
-	print("train_output_data_frame")
+	print('train_output_data_frame')
 	print(train_output_data_frame)
+	train_data_set = lightgbm.Dataset(train_input_data_frame, train_output_data_frame)
+	train_params = {'objective' : 'binary'}
+	model = lightgbm.train(train_params, train_data_set)
 
