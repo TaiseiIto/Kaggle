d700c12f698613ebe15591859f4e217a33e41bba
TaiseiIto <taisei1212@outlook.jp>
2023-12-12 16:49:34 +0900

    cats and dogs

--- competitions/cats-and-dogs-image-classification/train.py
+++ competitions/cats-and-dogs-image-classification/train.py
@@ -22,11 +22,9 @@ if __name__=='__main__':
 	train_image_path_ids = {train_image_path_id for train_image_path_id in train_image_path2train_image_path_id.values()}
 	train_image_path2label = {train_image_path : next(label for label in labels if train_image_path in label2train_image_paths[label]) for train_image_path in train_image_paths}
 	train_image_path_id2label_one_hot_vector = {train_image_path2train_image_path_id[train_image_path]: label2one_hot_vector[label] for train_image_path, label in train_image_path2label.items()}
-	train_image_path_id2train_image = {train_image_path2train_image_path_id[train_image_path] : numpy.array(Image.open(train_image_path).convert('RGB').resize(image_size)) / 0xff for train_image_path in train_image_paths}
+	train_image_path_id2train_image = {train_image_path2train_image_path_id[train_image_path] : numpy.array(Image.open(train_image_path).convert('RGB').resize(image_size)) for train_image_path in train_image_paths}
 	train_x = numpy.array([train_image_path_id2train_image[train_image_path_id] for train_image_path_id in sorted(train_image_path_ids)])
 	train_y = numpy.array([train_image_path_id2label_one_hot_vector[train_image_path_id] for train_image_path_id in sorted(train_image_path_ids)])
-	print(f"train_x.shape = {train_x.shape}")
-	print(f"train_y.shape = {train_y.shape}")
 
 	label2test_image_paths = {label : {test_image_path for test_image_path in (test_images / label).iterdir()} for label in labels}
 	test_image_paths = functools.reduce(lambda left_test_image_paths, right_test_image_paths : left_test_image_paths | right_test_image_paths, label2test_image_paths.values(), set())
@@ -34,9 +32,23 @@ if __name__=='__main__':
 	test_image_path_ids = {test_image_path_id for test_image_path_id in test_image_path2test_image_path_id.values()}
 	test_image_path2label = {test_image_path : next(label for label in labels if test_image_path in label2test_image_paths[label]) for test_image_path in test_image_paths}
 	test_image_path_id2label_one_hot_vector = {test_image_path2test_image_path_id[test_image_path]: label2one_hot_vector[label] for test_image_path, label in test_image_path2label.items()}
-	test_image_path_id2test_image = {test_image_path2test_image_path_id[test_image_path] : numpy.array(Image.open(test_image_path).convert('RGB').resize(image_size)) / 0xff for test_image_path in test_image_paths}
+	test_image_path_id2test_image = {test_image_path2test_image_path_id[test_image_path] : numpy.array(Image.open(test_image_path).convert('RGB').resize(image_size)) for test_image_path in test_image_paths}
 	test_x = numpy.array([test_image_path_id2test_image[test_image_path_id] for test_image_path_id in sorted(test_image_path_ids)])
 	test_y = numpy.array([test_image_path_id2label_one_hot_vector[test_image_path_id] for test_image_path_id in sorted(test_image_path_ids)])
-	print(f"test_x.shape = {test_x.shape}")
-	print(f"test_y.shape = {test_y.shape}")
+
+	model = tensorflow.keras.models.Sequential()
+	model.add(tensorflow.keras.layers.Rescaling(1 / 0xff, input_shape=train_x.shape[1:]))
+	model.add(tensorflow.keras.layers.Conv2D(32, (3, 3), activation = 'relu'))
+	model.add(tensorflow.keras.layers.MaxPooling2D(pool_size = (2, 2)))
+	model.add(tensorflow.keras.layers.Dropout(0.1))
+	model.add(tensorflow.keras.layers.Conv2D(64, (3, 3), activation = 'relu'))
+	model.add(tensorflow.keras.layers.MaxPooling2D(pool_size = (2, 2)))
+	model.add(tensorflow.keras.layers.Dropout(0.1))
+	model.add(tensorflow.keras.layers.Flatten())
+	model.add(tensorflow.keras.layers.Dense(0x100, activation = 'relu'))
+	model.add(tensorflow.keras.layers.Dropout(0.1))
+	model.add(tensorflow.keras.layers.Dense(len(labels), activation = 'softmax'))
+
+	model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = 'accuracy')
+	model.summary()
 
